120979a5e2fadbc1023692b77c425e36fea0fa87
gouravTheDev <chatterjeegouravking@gmail.com>
2022-03-01 00:11:09 +0530

    changes

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -6,15 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="" />
     <meta name="keywords" content="" />
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;70
+    0&family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('/css/cssf.css') }}" />
     <title>The Chezzy Slice</title>
     <style>
-        body {
-            width: 100%;
-            height: 100%;
+        html {
+            max-width: 100%;
             margin: 0px;
-            padding: 0px;
             overflow-x: hidden;
         }
     </style>
